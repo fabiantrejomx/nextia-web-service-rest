382eb061008215fc1365410d1923fc78d56159c4
ftrejo <fabiantrejo.p@gmail.com>
2021-12-29 17:23:47 -0600

    Create, delete and update endpoints were created.

--- src/main/java/com/fabian/rest/client/controller/TaskController.java
+++ src/main/java/com/fabian/rest/client/controller/TaskController.java
@@ -1,12 +1,22 @@
 package com.fabian.rest.client.controller;
 
 import com.fabian.rest.client.task.AllTaskResponse;
+import com.fabian.rest.client.task.DeleteTaskRequest;
+import com.fabian.rest.client.task.DeleteTaskResponse;
+import com.fabian.rest.client.task.TaskRequest;
+import com.fabian.rest.client.task.TaskResponse;
+import com.fabian.rest.client.task.UpdateTaskRequest;
+import com.fabian.rest.client.task.UpdateTaskResponse;
 import com.fabian.rest.client.wsclient.TaskClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +31,21 @@ public class TaskController {
     public ResponseEntity<AllTaskResponse> getAllTasksByUserId(@PathVariable("userId") final String userId){
         return ResponseEntity.ok(taskClient.getTasksByUserId(userId));
     }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TaskResponse> create(@RequestBody final TaskRequest request){
+        return ResponseEntity.ok(taskClient.create(request));
+    }
+
+    @DeleteMapping(value = "/{taskId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<DeleteTaskResponse> delete(@PathVariable("taskId") final String taskId){
+        return ResponseEntity.ok(taskClient.delete(taskId));
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UpdateTaskResponse> update(@RequestBody final UpdateTaskRequest request){
+        return ResponseEntity.ok(taskClient.update(request));
+    }
+
 
 }
--- src/main/java/com/fabian/rest/client/wsclient/TaskClient.java
+++ src/main/java/com/fabian/rest/client/wsclient/TaskClient.java
@@ -2,6 +2,12 @@ package com.fabian.rest.client.wsclient;
 
 import com.fabian.rest.client.task.AllTaskRequest;
 import com.fabian.rest.client.task.AllTaskResponse;
+import com.fabian.rest.client.task.DeleteTaskRequest;
+import com.fabian.rest.client.task.DeleteTaskResponse;
+import com.fabian.rest.client.task.TaskRequest;
+import com.fabian.rest.client.task.TaskResponse;
+import com.fabian.rest.client.task.UpdateTaskRequest;
+import com.fabian.rest.client.task.UpdateTaskResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -16,6 +22,7 @@ import org.springframework.ws.soap.client.core.SoapActionCallback;
 public class TaskClient {
 
     private final WebServiceTemplate template;
+    private static final String WS_URI = "http://localhost:8080/ws";
 
     public AllTaskResponse getTasksByUserId(final String userId) {
         final AllTaskRequest request = new AllTaskRequest();
@@ -23,9 +30,32 @@ public class TaskClient {
 
         log.info("Init request to get all tasks for userId {} ", userId);
         final AllTaskResponse response = (AllTaskResponse) template
-                .marshalSendAndReceive("http://localhost:8080/ws", request);
-
+                .marshalSendAndReceive(WS_URI, request);
         return response;
+    }
+
+
+    public TaskResponse create(final TaskRequest request){
+        log.info("Init request to create a new task");
+        final TaskResponse response = (TaskResponse) template
+                .marshalSendAndReceive(WS_URI, request);
+        return  response;
+    }
+
+    public DeleteTaskResponse delete(final String taskId){
+        final DeleteTaskRequest request = new DeleteTaskRequest();
+        request.setTaskId(taskId);
+        log.info("Init request to delete a task");
+        final DeleteTaskResponse response = (DeleteTaskResponse) template
+                .marshalSendAndReceive(WS_URI, request);
+        return  response;
+    }
+
+    public UpdateTaskResponse update(final UpdateTaskRequest request){
+        log.info("Init request to update a task");
+        final UpdateTaskResponse response = (UpdateTaskResponse) template
+                .marshalSendAndReceive(WS_URI, request);
+        return  response;
     }
 
 }
